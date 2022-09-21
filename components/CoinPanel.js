3419626cf4fd5b6637432cbfa68bb9e859c9ccc9
unenunciate <pmkelley@protonmail.com>
2022-09-21 12:00:24 -0400

    ui fixes

--- components/CoinPanel.js
+++ components/CoinPanel.js
@@ -35,22 +35,22 @@ const CoinPanel = ({coin}) => {
 
     const balance = getbalance();
 
-    const change = Math.abs(coin.holdings[0].quote_rate - coin.holdings[coin.holdings.length - 1].quote_rate / coin.holdings[coin.holdings.length - 1].quote_rate);
+    const change = (Math.abs(coin.holdings[0].quote_rate - coin.holdings[coin.holdings.length - 1].quote_rate / coin.holdings[coin.holdings.length - 1].quote_rate)) * 100;
 
     const chartColor = PRECENTAGE(change).format(true) === 0 ? '#4B5563' : coin.holdings[0].quote_rate > coin.holdings[coin.holdings.length - 1].quote_rate ? '#16A34A' : '#DC2626';
     const chartData = coin.holdings.map((holding, index) => ({ x: coin.holdings.length - index, y: USDNOSYM(holding.quote_rate).format(true) }));
 
     return(
-        <div className="flex flex-row w-full items-center justify-between py-2 px-6 bg-gray-800 border-2 border-purple-600 h-32 rounded-xl shadow-sm text-blue-400 shadow-purple-600">
-            <div className="flex flex-row w-1/5 justify-between items-center px-8 ">
-                <div className='rounded-full border-1 border-purple-600 h-8 w-8 shadow-sm shadow-purple-600'>
+        <div className="flex flex-row items-center justify-between w-full h-32 px-6 py-2 text-blue-400 bg-gray-800 border-2 border-purple-600 shadow-sm rounded-xl shadow-purple-600">
+            <div className="flex flex-row items-center justify-between w-1/5 px-8 min-w-fit">
+                <div className='w-8 h-8 border-purple-600 rounded-full shadow-sm border-1 shadow-purple-600'>
                     <Image className="w-full h-auto" src={coin.logo_url} width={32} height={32} />
                 </div>
                 
-                <span>{coin.contract_name}</span>
+                <span className='word'>{coin.contract_name}</span>
             </div>
 
-            <div className="flex flex-row w-1/5 justify-between items-center">
+            <div className="flex flex-row items-center justify-between w-1/5 min-w-[250px]">
                 <XYPlot width={180} height={96}>
                     <LineSeries
                         curve={'curveMonotoneX'}
@@ -64,11 +64,11 @@ const CoinPanel = ({coin}) => {
                 </span>
             </div>
 
-            <div className="flex flex-row w-1/5 justify-center items-center space-x-2">
+            <div className="flex flex-row items-center justify-center w-1/5 space-x-2">
                 <span>{USD(coin.holdings[0].quote_rate).format(true)}</span>
             </div>
 
-            <div className="flex flex-col w-1/5 justify-center items-center space-y-2">
+            <div className="flex flex-col items-center justify-center w-1/5 space-y-2">
                 <span>~ {CRYPTO(balance).format(true)}</span>
                 <span>{USD(coin.holdings[0].close.quote).format(true)}</span>
             </div>
