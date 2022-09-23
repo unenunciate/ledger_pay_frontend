389e31667c8e92dc86f632c97217b56a20d19922
unenunciate <pmkelley@protonmail.com>
2022-09-23 02:30:18 -0400

    added all required fields for signup and edit url

--- pages/connect/signup.js
+++ pages/connect/signup.js
@@ -6,17 +6,20 @@ import useAuth from '../../hooks/useAuth';
 import axios from 'axios';
 
 const Signup = () => {
-  const { user, updateUser } = useAuth();
+  const { user, updateUser, isConnected, triggerEthereumLogin } = useAuth();
 
   const router = useRouter();
 
   const [username, setUsername] = useState('');
-  const [name, setName] = useState('');
-
-  const [credential, setCredential] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [birthday, setBirthday] = useState('');
 
   const [invalidEmail, setInvalidEmail] = useState(true);
   const [invalidPhone, setInvalidPhone] = useState(true);
+  const [invalidBirthday, setInvalidBirthday] = useState(true);
 
   const isValidEmail = (emailValue) => {
     const regex = /\S+@\S+\.\S+/;
@@ -25,28 +28,17 @@ const Signup = () => {
 
     String(emailValue).length > 5 ? result = regex.test(emailValue) : result = false;
 
-    return result;
+    setInvalidEmail(!result);
   };
 
   const isValidPhone = (phoneValue) => {
     const regex = /(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?/g;
-    return regex.test(phoneValue);
+    setInvalidPhone(!regex.test(phoneValue));
   };
 
-  const onCredentialChange = (c) => {
-    setCredential(c);
-
-    if (isValidEmail(c)) {
-      setInvalidEmail(false);
-    } else {
-      setInvalidEmail(true);
-    }
-
-    if (isValidPhone(c)) {
-      setInvalidPhone(false);
-    } else {
-      setInvalidPhone(true);
-    }
+  const isValidBirthday = (birthdayValue) => {
+    const regex = /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/;
+    setInvalidBirthday(!regex.test(birthdayValue));
   };
 
   const onSignupSubmit = async (e) => {
@@ -54,17 +46,19 @@ const Signup = () => {
 
     let res;
 
-    if(!invalidEmail || !invalidPhone) {
-      res = await axios.post(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/stytches/createUserWithCredential?credential=${credential}`, invalidEmail ? {phone: credential, username, name, pasword: "notusedXXAS"} : {email: credential, username, name, pasword: "notusedXXAS"});
+    res = await axios.post(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/strapi-stytch-provider/createStytch`, {stytchUUID: res.data.user_id});
+
+    if(!invalidEmail || !invalidPhone || !invalidBirthday) {
+      res = await axios.post(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/strapi-stytch-provider/createUserFromStytch`, {username, firstName, lastName, email, phone, birthday, stytches: [res.data.id]});
     }
     
-    if(res?.data) {
-      updateUser(res.data);
+    if(res?.user?.username) {
+      updateUser(res.data.user);
     }
   };
 
   useEffect(() => {
-    if(user?.id) {
+    if(isConnected()) {
       router.push('/');
     } 
   }, [user])
@@ -73,16 +67,28 @@ const Signup = () => {
     <div className='flex flex-col items-center justify-center w-full h-screen bg-gradient-to-b from-purple-600 to-blue-400'>
       <section className='flex flex-col items-center justify-center w-5/6 py-6 pb-6 bg-gray-800 shadow-xl rounded-xl md:w-3/4 lg:w-1/2 2xl:w-1/3 shadow-gray-800'>
         <Link href={"https://www.ledgepay.io"}><a className="flex items-center h-full px-6 rounded-lg hover:bg-gray-900"><Image width={32} height={32} src="/LedgePay.png" layout="fixed" /></a></Link>
-
-        <div className='flex items-center w-3/4 h-16 mb-4'/>
        
         <div div className='w-3/4 h-full space-y-6'>
           <form onSubmit={onSignupSubmit} className={`w-full flex flex-col space-y-12 justify-center items-center ${false ? 'invisiable' : ''}`}>
               <input
                 className={`h-full px-2 py-2 text-center text-blue-400 placeholder-blue-400 bg-gray-700 border-none rounded-lg w-full form-input focus:border-none focus:outline-none`}
-                placeholder='Email or Phone'
-                value={credential}
-                onChange={(e) => onCredentialChange(e.target.value)}
+                placeholder='Email'
+                value={email}
+                onChange={(e) => {
+                  isValidEmail(e.target.value);
+                  setEmail(e.target.value);
+                }}
+                type="text"
+              />
+
+            <input
+                className={`h-full px-2 py-2 text-center text-blue-400 placeholder-blue-400 bg-gray-700 border-none rounded-lg w-full form-input focus:border-none focus:outline-none`}
+                placeholder='Phone'
+                value={phone}
+                onChange={(e) => {
+                  isValidPhone(e.target.value);
+                  setPhone(e.target.value);
+                }}
                 type="text"
               />
 
@@ -96,13 +102,36 @@ const Signup = () => {
 
               <input
                 className={`h-full px-2 py-2 text-center text-blue-400 placeholder-blue-400 bg-gray-700 border-none rounded-lg w-full form-input focus:border-none focus:outline-none`}
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
                 type="text"
               />
 
-              <input disabled={(!invalidEmail || !invalidPhone) || name.length < 3 || username.length < 3 } className='relative w-2/3 py-2 font-bold text-blue-400 bg-purple-600 shadow-md shadow-purple-600 hover:shadow-purple-300 rounded-xl text-bold hover:border-pruple-300 hover:text-blue-100 hover:bg-purple-300 active:scale-75' type="submit" value="Submit" />
+              <input
+                className={`h-full px-2 py-2 text-center text-blue-400 placeholder-blue-400 bg-gray-700 border-none rounded-lg w-full form-input focus:border-none focus:outline-none`}
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                type="text"
+              />
+
+            <input
+                className={`h-full px-2 py-2 text-center text-blue-400 placeholder-blue-400 bg-gray-700 border-none rounded-lg w-full form-input focus:border-none focus:outline-none`}
+                placeholder='Birthday'
+                value={birthday}
+                onChange={(e) => {
+                  isValidBirthday(e.target.value);
+                  setBirthday(e.target.value);
+                }}
+                type="text"
+              />
+
+              <button onClick={() => triggerEthereumLogin()} className={`relative w-2/3 shadow-sm font-bold shadow-purple-600 hover:shadow-purple-300 text-blue-400 py-2 bg-purple-600 rounded-lg text-bold hover:border-pruple-300 hover:text-blue-100 hover:bg-purple-300 active:scale-75`} >
+                <span className='w-full text-center'>Wallet</span>
+              </button>
+
+              <input disabled={(!invalidEmail || !invalidPhone || invalidBirthday) || firstName.length < 3 || lastName.length < 3 || username.length < 3 } className='relative w-2/3 py-2 font-bold text-blue-400 bg-purple-600 shadow-md shadow-purple-600 hover:shadow-purple-300 rounded-xl text-bold hover:border-pruple-300 hover:text-blue-100 hover:bg-purple-300 active:scale-75' type="submit" value="Submit" />
             </form>
         </div>
       </section>
