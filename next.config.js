c3059004bc29c360e05d90b40791e6a31c63cfd8
unenunciate <pmkelley@protonmail.com>
2022-09-25 02:44:27 -0400

    err build dammit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['strapi.ledgerpay.io', 'pbs.twimg.com', 'logos.covalenthq.com', 'dynamic-assets.coinbase.com'],
   },
